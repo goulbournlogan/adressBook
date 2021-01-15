e2556e5b95075e23c7fb8500d06016a46e8a88b3
Logan Goulbourn <goulbournlogan@gmail.com>
2021-01-15 12:22:04 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+class App extends React.Component {
+  state={
+    phoneNumbers: [],
+    inputText: ""
+  }
+  handleChange=(event) => {
+    this.setState({inputText: event.target.value})
+    console.log(this.state.inputText)
+  }
+  handleSubmit=(event) => {
+    event.preventDefault();
+    this.setState({
+      phoneNumbers: [...this.state.phoneNumbers,this.state.inputText],
+      inputText: "",
+    }); 
+  };
+  render() {
+    return (
+        <div className="container">
+          <h1>Phone Book</h1>
+          <h2>Type number to save</h2>
+          <form onSubmit={this.handleSubmit}> 
+          <input type="text" value={this.state.inputText} onChange={this.handleChange}/>
+          {this.state.phoneNumbers.map((numbers) => {
+            return(
+              <h1>{numbers}</h1>
+            )
+          })} 
+          </form> 
+        </div> 
+    )
+  }
 }
 
 export default App;
